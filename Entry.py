ea0104e97d7be5a23eaa3f4809cec4dc4b601f3f
luke <janik.l@husky.neu.edu>
2018-06-13 12:36:09 -0400

    entry

---
+++ Entry.py
@@ -0,0 +1,12 @@
+class Entry(object):
+    def __init__(self, name, gender, age, city, time):
+        self.name = str(name)
+        self.gender = str(gender)
+        self.age = str(age)
+        self.city = str(city)
+        self.time = str(time)
+
+    def __str__(self):
+        return "Name: " + self.name + "\nGender: " + self.gender + "\nAge: " + self.age + \
+               "\nCity " + self.city + "\nTime " + self.time + "\n"
+
